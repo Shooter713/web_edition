81e1205206c81fd3e7b5e3e954ef7f30f1e0789c
Shooter713 <shooter7ua@gmail.com>
2023-05-03 14:39:30 +0300

    Додавання міграцї

---
+++ database/migrations/2023_04_27_143334_create_table_tags.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tags', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+        });
+
+        Schema::create('news_tags', function (Blueprint $table) {
+            $table->id();
+            $table->integer('news_id');
+            $table->integer('tags_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tags');
+        Schema::dropIfExists('news_tags');
+    }
+};
